a95bf60db61daa23fe974cf2bec72f49f205144c
akhila1432 <88018145+akhila1432@users.noreply.github.com>
2021-09-08 08:21:47 +0530

    Delete counter method.py

--- .vscode/counter method.py
+++
@@ -1,8 +0,0 @@
-user_input = input("hey enter the number of days to you want to calculate?\n")
-user_input_number = int(user_input)
-calculation_to_units = 24
-name_of_units = "hours"
-def days_to_units(number_of_days):
- return f"{number_of_days} days {number_of_days *calculation_to_units} {name_of_units}"
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
